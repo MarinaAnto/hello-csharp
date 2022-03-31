c03a3bcbd3c88ec7ce74181ae9a9a0594bdfcb62
MarinaAnto <94830694+MarinaAnto@users.noreply.github.com>
2022-03-31 17:10:32 +0200

    Esercizio
    Non so perchè non si è inviato

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ Scrivere un programma che stampi un messaggio di Benvenuto.
+Successivamente stampa i mesi dell’anno riga per riga e chiede all’utente di scrivere il suo mese preferito.
+Una volta inserito il mese preferito, il programma stampa un messaggio di saluto e termina.
+*/
+Console.WriteLine("Benvenuto !!!");
+string[] months = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto","Settembre","Ottobre","Novembre","Dicembre"};
+foreach (string month in months)
+{
+    Console.WriteLine(month);
+}
+Console.WriteLine("Qual'è il tuo mese preferito?");
+ string prefer = Console.ReadLine();
+Console.WriteLine("Il tuo mese preferito è " + prefer   + ". Arrivederci !!!");
